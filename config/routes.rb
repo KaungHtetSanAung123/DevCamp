45f0ef61240215c04e3a8bc0846d0dc2aedbc409
Kaung Htet <khsa16@gmail.com>
2023-02-28 13:00:04 +0630

    Added session tracker for dynamic source links

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'pages/home'
+  get 'portfolio/:id', to: 'portfolios#show' ,as: 'portfolio_show'
 
+  #get 'pages/home'
   get 'about', to: 'pages#about'
+  #get 'pages/contact'
   get 'contact', to: 'pages#contact'
-
-
-  resources :blogs
   
-  root to: 'pages#home'
+  resources :blogs do 
+    member do
+      get :toggle_status
+    end
+  end
+  
+  root 'pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_091328) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_074950) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
-    t.integer  "status",  default: 0
-    t.index ["slug"],name: "index_blogs_on_slugs", unique: true, using: :btree
+    t.string "slug"
+    t.integer "status"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "guides", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -40,13 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_091328) do
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
