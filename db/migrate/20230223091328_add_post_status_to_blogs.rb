a98455907aa2bbb23a8e929727404bf21db193cb
Kaung Htet <khsa16@gmail.com>
2023-03-13 11:46:05 +0630

    Integrated  database relationship  between topic and blog posts

--- db/migrate/20230223091328_add_post_status_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddPostStatusToBlogs < ActiveRecord::Migration[7.0]
-  def change
-    add_column :blogs, :status, :integer, default: 0
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_091328) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_045550) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "slug"
-    t.integer  "status",  default: 0
-    t.index ["slug"],name: "index_blogs_on_slugs", unique: true, using: :btree
+    t.string "slug"
+    t.integer "status"
+    t.integer "topic_id"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -26,6 +28,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_091328) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "guides", force: :cascade do |t|
@@ -58,4 +71,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_091328) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
